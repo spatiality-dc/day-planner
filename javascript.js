c45ee05ed886f1f2ea8d1a1df76bc63980039647
spatiality-dc <dan@dinosaurmedia.org>
2020-03-08 21:33:27 +1100

    added in js colour change functions

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 var dateAndTime = document.querySelector("#time-of-day");
 var currentTime;
 renderTime();
+colourChanges();
 
 function renderTime() {
   currentTime = document.createElement("h3");
@@ -13,4 +14,18 @@ function liveTime() {
   currentTime.textContent =
     moment().format("dddd") + ", " + moment().format("MMMM Do YYYY, h:mm a");
   setInterval(liveTime, 30000);
+}
+
+function colourChanges() {
+  $(".time-block").each(function() {
+    const now = parseInt(moment().format("H"));
+    const span = parseInt($(this).attr("data-time"));
+    if (span < now) {
+      $(this).attr("class", "past");
+    } else if (now == span) {
+      $(this).attr("class", "present");
+    } else {
+      $(this).attr("class", "future");
+    }
+  });
 }

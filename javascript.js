31f663d2380ffb866483c0b59c7a0f64b6743f0e
spatiality-dc <dan@dinosaurmedia.org>
2020-03-08 20:58:59 +1100

    trying to configure mobile size html and css

--- javascript.js
+++ javascript.js
@@ -1,21 +1,5 @@
 // Display current date with the currentDay id, header 3rd child
 
-const m = moment().format("dddd MMM Mo YYYY");
-
-console.log(m);
-
-//In css .time-block
-
-//Display 9 hour blocks, between 9am and 5pm, class name .row
-
-//Blocks need to be editable, text input
-
-//lhs has class of .hour
-
-//rhs has class of .saveBtn
-
-//save button locks rows, cannot be edited once locked
-
 //rows in past cannot be edited
 
 //rows in past become grey, times in past have class of .past
